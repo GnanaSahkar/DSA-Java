e540b4287d893b009b54df6b77576e83628ff7c8
GnanaSahkar <gnanasahkar123@gmail.com>
2024-06-04 11:13:33 +0530

    bubbleSort

--- bubblesort.java
+++ bubblesort.java
@@ -4,7 +4,7 @@ public class bubblesort {
 
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
-                if(arr[i]<arr[j]){
+                if(arr[i]<arr[j]){     // < asscending Order > desscending order
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
