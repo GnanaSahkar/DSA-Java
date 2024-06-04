6be6cc0b1c3a4e206fa0aae61f3f233616a1ca79
GnanaSahkar <gnanasahkar123@gmail.com>
2024-06-04 12:02:53 +0530

    Printing Linked List in Different Ways

--- LList.java
+++ LList.java
@@ -1,14 +1,41 @@
 import java.util.LinkedList;
 
+import java.util.*;
+
 public class LList {
     public static void main(String[] args) {
-        
+        //initialise
         LinkedList<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(5);
+        list.add(2);
         list.add(4);
+        list.add(-1);
+        list.add(9);
 
-        list.forEach(System.out:: println);
-    
+        /*System.out.println(list.toString());
+
+        // print
+        for (int i : list) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(list.get(i));
+        }
+
+        int count = 0;
+        while (count < list.size()) {
+            System.out.println(list.get(count));
+            count++;
+        }
+
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }*/
+
+        // list.forEach(element -> System.out.println(element));
+        list.forEach(System.out::println);
+
+        // Java 8 features
     }
 }
