ffc0313e83d94cd69c6c1773819f78479f2b64fc
GnanaSahkar <gnanasahkar123@gmail.com>
2024-06-04 11:11:58 +0530

    bubbleSort

---
+++ bubblesort.java
@@ -0,0 +1,18 @@
+public class bubblesort {
+    public static void main(String[] args) {
+        int[] arr = {-1,0,9,11,18};
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]<arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int i:arr){
+            System.out.print(i +" ");
+        }
+    }
+}
