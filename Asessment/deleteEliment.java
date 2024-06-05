f633fe6483a79b9c7ac6e1907e8e90cf18fb641d
GnanaSahkar <gnanasahkar123@gmail.com>
2024-06-05 09:28:15 +0530

    Assessments

---
+++ Asessment/deleteEliment.java
@@ -0,0 +1,27 @@
+package Asessment;
+
+import java.util.*;
+
+public class deleteEliment {
+    public static void main(String[] args) {
+        LinkedList <Integer> list = new LinkedList<>();
+
+        list.add(-3);
+        list.add(-1);
+        list.add(10);
+        list.add(18);
+        list.add(5);
+        list.add(-7);
+        list.add(-6);
+        
+        for (int i : list) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        list.remove(list.size()/2);
+        for (int i : list) {
+            System.out.print(i+" ");
+        }
+        
+    }
+}
---
+++ Asessment/reverseList.java
@@ -0,0 +1,31 @@
+package Asessment;
+
+ import java.util.*;
+public class reverseList {
+  
+    public static void main(String[] args) {
+        
+      LinkedList <Integer> list = new LinkedList<>();
+
+
+        list.add(-3);
+        list.add(-1);
+        list.add(10);
+        list.add(18);
+        list.add(5);
+        list.add(-7);
+        list.add(-6);
+
+        System.out.println("NormalList");
+        for (Integer integer : list) {
+            System.out.print(integer +" ");
+        }
+        System.out.println();
+        System.out.println("Reversed LinkedList");
+        Collections.reverse(list);
+        for (Integer integer : list) {
+            System.out.print(integer +" ");
+        }
+    }
+}
+
