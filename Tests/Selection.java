cd9a69e56f6a78fe5ea797e873d96cff639dad5c
GnanaSahkar <gnanasahkar123@gmail.com>
2024-06-11 13:40:14 +0530

    selectionSort

---
+++ Tests/Selection.java
@@ -0,0 +1,25 @@
+package Tests;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 3};
+        int []result = selectionSort(arr);
+        for(int i:result){
+            System.out.print(i+" ");
+        }
+    }
+    public static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        return arr;
+    }
+}
